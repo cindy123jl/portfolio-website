74260e74194fe85d09ad134f399baef6b2cf19ee
Cindy Lara <cindy123jl@gmail.com>
2025-04-13 14:40:09 -0700

    trying a video as a hero

--- cindy-web-dev-portfolio/src/index.js
+++ cindy-web-dev-portfolio/src/index.js
@@ -5,6 +5,7 @@ import "./index.css";
 function App() {
   return (
     <div className="container">
+      <Hero />
       <Header />
     </div>
   );
@@ -14,32 +15,39 @@ function Header() {
   //look for font
   return (
     <header>
-      <nav class="navbar bg-body-tertiary">
+      <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
           <a class="navbar-brand" href="./">
-            <div class="container">
-              <div class="row">
-                <div class="col">
-                  <img
-                    src="logo/artmeetcindylogo.png"
-                    alt="Logo"
-                    width="30"
-                    height="30"
-                    class="d-inline-block align-text-top"
-                  />
-                </div>
-                <div class="col">
-                  <p className="header-title">
-                    Cindy's Web Development Porfolio
-                  </p>
-                </div>
-                <div class="col">hi</div>
+            <div class="row">
+              <div class="col">
+                <img
+                  src="logo/artmeetcindylogo.png"
+                  alt="Logo"
+                  width="40"
+                  height="40"
+                  class="d-inline-block align-text-top"
+                />
+              </div>
+              <div class="col">
+                <p className="header-title">Cindy's Web Development Porfolio</p>
               </div>
             </div>
           </a>
         </div>
       </nav>
     </header>
+  );
+}
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div class="container-fluid">
+        <video width="320" height="240">
+          <source src="videos/portfolio-video.mp4"></source>
+        </video>
+      </div>
+    </div>
   );
 }
 
